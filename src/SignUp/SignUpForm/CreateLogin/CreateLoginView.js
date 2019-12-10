09f3abb846553755efbe7776001d0beef2d1fb38
lgdelacruz <lgdelacruz@lgdelacruz.com>
2019-12-10 15:28:29 -0800

    Added Email and password field

--- src/SignUp/SignUpForm/CreateLogin/CreateLoginView.js
+++ src/SignUp/SignUpForm/CreateLogin/CreateLoginView.js
@@ -5,7 +5,8 @@ import * as MUI from '@material-ui/core';
 const CreateLoginView = props => {
   return (
     <React.Fragment>
-      <MUI.TextField variant="outlined"/>
+      <MUI.TextField fullWidth variant="outlined" label="Email"/>
+      <MUI.TextField fullWidth variant="outlined" label="Password"/>
     </React.Fragment>
   );
 };
