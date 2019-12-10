19d5322c774c305d5b8d3fba14457ca1e75dcfe0
lgdelacruz <lgdelacruz@lgdelacruz.com>
2019-12-10 15:00:27 -0800

    Added Branding and Sign Up

---
+++ src/SignUp/Branding/Branding.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Branding = props => {
+  return (
+    <div></div>
+  );
+};
+
+export default Branding;
---
+++ src/SignUp/SignUpForm/SignUpForm.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SignUpForm = props => {
+  return (
+    <div></div>
+  );
+};
+
+export default SignUpForm;
