97d78eb4707740ea7526e38ec35455a2e55a567d
lgdelacruz <lgdelacruz@lgdelacruz.com>
2019-12-11 11:05:39 -0800

    Stress testing validate email

--- src/SignUp/SignUpForm/Forms/CreateLogin/helpers/validateEmail.test.js
+++ src/SignUp/SignUpForm/Forms/CreateLogin/helpers/validateEmail.test.js
@@ -3,6 +3,13 @@ import { validateEmail } from ".";
 describe("Validate Email Test", () => {
   test("Validate different types of emails", () => {
     expect(validateEmail("a@a.com")).toBe(true);
-    expect(validateEmail("a@a.")).toBe(false);
+    expect(validateEmail("a@b.c")).toBe(true);
+    expect(validateEmail("1@a.com")).toBe(true);
+
+    expect(validateEmail("a@.az")).toBe(false);
+    expect(validateEmail("")).toBe(false);
+    expect(validateEmail("a")).toBe(false);
+    expect(validateEmail("@ad.com")).toBe(false);
+    expect(validateEmail("@ad.123")).toBe(false);
   });
 });
