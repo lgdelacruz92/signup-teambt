c4165e69839c702b6ca8052dc700586b67d4be72
lgdelacruz <lgdelacruz@lgdelacruz.com>
2019-12-11 11:03:20 -0800

    Initial email validation test

--- src/SignUp/SignUpForm/Forms/CreateLogin/helpers/validateEmail.test.js
+++ src/SignUp/SignUpForm/Forms/CreateLogin/helpers/validateEmail.test.js
@@ -3,5 +3,6 @@ import { validateEmail } from ".";
 describe("Validate Email Test", () => {
   test("Validate different types of emails", () => {
     expect(validateEmail("a@a.com")).toBe(true);
+    expect(validateEmail("a@a.")).toBe(false);
   });
 });
