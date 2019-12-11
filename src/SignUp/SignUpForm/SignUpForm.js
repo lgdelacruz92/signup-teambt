d2cb84b7f4d5bcf325560b007b7d306c97666fd0
lgdelacruz <lgdelacruz@lgdelacruz.com>
2019-12-11 08:40:50 -0800

    Added storybook

--- src/SignUp/SignUpForm/SignUpForm.js
+++ src/SignUp/SignUpForm/SignUpForm.js
@@ -1,33 +1,35 @@
 import React from "react";
-import SignUpFormArea from './SignUpFormArea'
+import SignUpFormArea from "./SignUpFormArea";
 import SignUpFormContainer from "./SignUpFormContainer";
-import SignUpFormTitle from './SignUpFormTitle'
+import SignUpFormTitle from "./SignUpFormTitle";
 import FormControls from "./FormControls";
-import Forms from './Forms'
+import Forms from "./Forms";
 
 const SignUpForm = props => {
   const [formState, setFormState] = React.useState({
-    email: '',
-    password: '',
-    firstName: '',
-    lastName: '',
-    companyName: '',
-    website: '',
-    phoneNumber: ''
+    email: "",
+    password: "",
+    firstName: "",
+    lastName: "",
+    companyName: "",
+    website: "",
+    phoneNumber: ""
   });
-  const [currentForm, setCurrentForm] = React.useState('CreateLogin');
+  const [currentForm, setCurrentForm] = React.useState("CreateLogin");
   return (
     <SignUpFormArea>
       <SignUpFormContainer>
         <SignUpFormTitle />
-        <Forms 
-          formState={formState} 
-          setFormState={setFormState} 
-          currentForm={currentForm}/>
-        <FormControls 
+        <Forms
+          formState={formState}
+          setFormState={setFormState}
           currentForm={currentForm}
-          onForward={forwardForm => setCurrentForm(forwardForm)} 
-          onBack={backForm => setCurrentForm(backForm)}/>
+        />
+        <FormControls
+          currentForm={currentForm}
+          onForward={forwardForm => setCurrentForm(forwardForm)}
+          onBack={backForm => setCurrentForm(backForm)}
+        />
       </SignUpFormContainer>
     </SignUpFormArea>
   );
